c118da0688a374521b62f22975bef49f391b65df
edpadua <ed.padua@gmail.com>
2023-09-25 22:03:31 -0300

    Update 3
    Update 3

--- 4matt/src/Components/Graphics/InvoiceSpendOvertime/index.jsx
+++ 4matt/src/Components/Graphics/InvoiceSpendOvertime/index.jsx
@@ -4,20 +4,64 @@ import { DataContext } from "../../../Context/Data";
 import GraphicContainer from "../GraphicContainer";
 
 function InvoiceSpendOvertime() {
-  const { data } = useContext(DataContext);
+  const { data, monthsYear, monthsYearObj } = useContext(DataContext);
 
-  const [monthYearInvoice, setMonthYearInvoice] = useState({});
+  const [monthYearInvoice, setMonthYearInvoice] = useState([]);
 
-  const getMonthYearInvoice = () => {
-    
-    console.log("Filter", data);
-  };
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, "0");
+  }
 
   useEffect(() => {
+    const getMonthYearInvoice = () => {
+      console.log("Filter", data);
+      const newMonthYearInvoice = monthYearInvoice;
+      console.log("Temp2", newMonthYearInvoice);
+      monthsYearObj.map(function calcInvoice(item) {
+        let sum = 0;
+        console.log("Temp1", monthYearInvoice);
+        
+        console.log("newMonthYearInvoice", newMonthYearInvoice);
+        console.log("item", item);
+        setMonthYearInvoice([]);
+        
+        /*data.forEach((element) => {
+          if (
+            item["0"] == padTo2Digits(new Date(element.Date).getMonth() + 1) &&
+            item["1"] == new Date(element.Date).getFullYear()
+          ) {
+            
+           
+           
+            sum += Number(element.Spend);
+            
+          }
+         
+        });*/
+       
+        newMonthYearInvoice.push({
+          month: item["0"],
+          Year: item["1"],
+          IncomeTotal: 2,
+        });
+        console.log("Push");
+        setMonthYearInvoice(newMonthYearInvoice);
+       
+      });
+     
+    };
+    console.log("Temp", monthYearInvoice);
     getMonthYearInvoice();
-  }, []);
+  }, [data,monthYearInvoice,setMonthYearInvoice]);
 
-  return <GraphicContainer title="Invoice Spend Overtime"></GraphicContainer>;
+  return (
+    <>
+      {data
+        ? data.map((item, index) => <h2 key={index}>{item.Date}</h2>)
+        : "Loading"}
+      <GraphicContainer title="Invoice Spend Overtime"></GraphicContainer>
+    </>
+  );
 }
 
 export default InvoiceSpendOvertime;
--- 4matt/src/Context/Data.jsx
+++ 4matt/src/Context/Data.jsx
@@ -8,6 +8,62 @@ DataContext.displayName = "Data";
 
 export default function DataProvider({ children }) {
   const [data, setData] = useState([]);
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [dates, setDates] = useState([]);
+  const [monthsYear, setMonthsYear] = useState([]);
+  const [monthsYearObj, setMonthsYearObj] = useState([]);
+  const [Months, setMonths] = useState([]);
+  const [Years, setYears] = useState([]);
+
+  function selectDates(data) {
+    const { Date } = data;
+    return Date;
+  }
+
+  function monthYearList(month, year) {
+    const obj = Object.fromEntries(
+      month.map((key, index) => [key, year[index]])
+    );
+
+    return obj;
+  }
+
+  function dateComparison(a, b) {
+    const date1 = new Date(a);
+    const date2 = new Date(b);
+
+    return date1 - date2;
+  }
+
+  function padTo2Digits(num) {
+    return num.toString().padStart(2, "0");
+  }
+
+  function removeDuplicates(array) {
+    
+    let newArray = [];
+
+    
+    let uniqueObject = {};
+
+    
+    for (let i in array) {
+     
+      let objTitle = array[i][0];
+
+     
+      uniqueObject[objTitle] = array[i];
+    }
+
+    
+    for (let i in uniqueObject) {
+      newArray.push(uniqueObject[i]);
+    }
+
+    
+    return newArray;
+  }
 
   const getData = async () => {
     console.log("Get data");
@@ -16,7 +72,53 @@ export default function DataProvider({ children }) {
       const resp = await axios.get("http://localhost:3000/data", {
         headers: { "Content-Type": "application/json" },
       });
-      console.log("data",resp.data);
+      const onlyDates = [...new Set(resp.data.map(selectDates))];
+      const onlyDatesDates = onlyDates.map(
+        (dateString) => new Date(dateString)
+      );
+      onlyDatesDates.sort(dateComparison);
+      const onlyDatesOrdered = onlyDatesDates.map(
+        (date) =>
+          padTo2Digits(date.getMonth() + 1) +
+          "/" +
+          padTo2Digits(date.getDate()) +
+          "/" +
+          date.getFullYear()
+      );
+      const onlyMonthsYearOrdered = [
+        ...new Set(
+          onlyDatesDates.map(
+            (date) =>
+              padTo2Digits(date.getMonth() + 1) + "/" + date.getFullYear()
+          )
+        ),
+      ];
+      const onlyMonthsOrdered = onlyDatesDates.map((date) =>
+        padTo2Digits(date.getMonth() + 1)
+      );
+      const onlyYearsOrdered = onlyDatesDates.map((date) =>
+        padTo2Digits(date.getFullYear())
+      );
+      const monthYear = onlyMonthsOrdered.map(function (value, index) {
+        const temp = [value, onlyYearsOrdered[index]];
+        return { ...temp };
+      });
+
+      const monthYearUnique=removeDuplicates(monthYear);
+
+   
+      console.log("data", resp.data);
+      console.log("onlyDates", onlyDates);
+      console.log("onlyDatesDates", onlyDatesDates);
+      console.log("onlyDatesOrdered", onlyDatesOrdered);
+      console.log("onlyMonthsOrdered", onlyMonthsOrdered);
+      console.log("onlyYearsOrdered", onlyYearsOrdered);
+      console.log("onlyMonthsOrdered", onlyMonthsYearOrdered);
+      console.log("MonthYear", monthYear);
+      console.log("monthYearUnique", monthYearUnique);
+      setDates(onlyDatesOrdered);
+      setMonthsYear(onlyMonthsYearOrdered);
+      setMonthsYearObj(monthYearUnique);
       setData(resp.data);
     } catch (error) {
       console.log(error);
@@ -31,6 +133,8 @@ export default function DataProvider({ children }) {
     <DataContext.Provider
       value={{
         data,
+        monthsYear,
+        monthsYearObj,
       }}
     >
       {children}
